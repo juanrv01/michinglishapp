21676ad6e736c0c941f646b873d2a34a644e6cf4
Fredy Ballesteros <fredy.ballesteros@correounivalle.edu.co>
2025-04-20 18:17:31 -0500

    Adding testing and fixing formatting code

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { dataSlice } from "./data"
 import { linkwordsSlice } from "./linkwords";
+import { verbsSlice } from "./verbs";
 
 
 export const store = configureStore({
     reducer: {
         data: dataSlice.reducer,
-        linkwords: linkwordsSlice.reducer
+        linkwords: linkwordsSlice.reducer,
+        verbs: verbsSlice.reducer
     }
 })
